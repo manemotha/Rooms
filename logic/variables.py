a1ddd0372eabbeb1f70dea243dda9af1bb33d877
Mane Motha <motha.manelisi@gmail.com>
2022-06-02 16:28:40 +0200

    project: create new "variables.py" file to store global string variables

---
+++ logic/variables.py
@@ -0,0 +1,17 @@
+# account & account-dir
+account_generated_true: str = "account generated: true"
+account_deactivated_true: str = "account deactivate: true"
+account_exists_true: str = "account exists: true"
+account_exists_false: str = "account exists: false"
+account_dir_exists: str = "account dir exists: true but not account"
+account_dir_exists_restored: str = "account dir exists: true but not account \"directory removed\""
+# access
+account_access_granted: str = "access granted"
+account_access_denied_password: str = "access denied: incorrect password"
+account_access_denied_passwordhash: str = "access denied: password not hashed \"account removed\""
+# character
+username_unwanted_character: str = "unwanted character in username"
+
+# directory
+# accounts parent directory
+databases_directory = "storage/database/accounts.db"
