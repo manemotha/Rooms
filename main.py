bf52a17237510a6ad26faa19ac6c324f77d567e2
Mane Motha <motha.manelisi@gmail.com>
2022-06-02 15:19:55 +0200

    main.py: server setup

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 from __init__ import *
+
+
+# server host address ws://HOST:PORT/
+HOST: str = '0.0.0.0'
+PORT: str = '5000'
+
+
+# server connection gateway
+async def index(websocket, namespace: str):
+    pass
+
+
+# application server
+async def server():
+    async with websockets.serve(index, HOST, PORT):
+        await asyncio.Future()  # run forever
+
+
+# start application server
+if __name__ == '__main__':
+    try:
+        print(f'[SERVER]: Listening on -> ws://{HOST}:{PORT}/')
+        asyncio.run(server())
+    except KeyboardInterrupt:
+        print('\n[SERVER]: Server forced to stop')
