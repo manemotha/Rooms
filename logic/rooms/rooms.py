9335a2a888f2f6a2463340e05b87fc634f3f47fd
Mane Motha <motha.manelisi@gmail.com>
2022-08-03 18:12:09 +0200

    rooms: update database from Sqlite to MongoDB

--- logic/rooms/rooms.py
+++ logic/rooms/rooms.py
@@ -10,40 +10,40 @@ class Rooms:
         self.password: str = self.account['password']
         self.table_name: str = users_table_name
 
+        # mongodb server connection
+        self.mongodb_connection = pymongo.MongoClient(database_uri, serverSelectionTimeoutMS=500)
+
+        # database user's account table
+        self.table_name = users_table_name
+        self.database_name = users_database_name
+
     async def new_room(self, room: dict):
-        room_title: str = room['title']
+        # generate unique id from room-title
+        # this id is used for CRUD functionalities on this room
+        room['id']: str = str(id(room['title']))
 
         try:
-            database = sqlite3.connect(f'{database_directory}/accounts.db')
-            cursor = database.cursor()
+            self.mongodb_connection.server_info()
+            # create database connection
+            database = self.mongodb_connection[self.database_name]
+            # connect to table
+            table = database.get_collection(self.table_name)
 
             authentication_result = await Account(self.account).authenticate()
 
             if authentication_result['result'] == account_access_granted:
+                # update / create new room value
+                table.update_one({"username": self.username}, {"$push": {"rooms": room}})
+
                 # get all local rooms
-                local_rooms: dict = json.loads(cursor.execute(f"""
-                SELECT json_extract(room, '$') FROM {self.table_name} WHERE json_extract(account, '$.username')='{self.username}';
-                """).fetchone()[0])
+                local_rooms: list = table.find_one({"username": self.username})["rooms"]
 
-                # generate random id from room-title
-                room_id: str = str(id(room_title))
-
-                # insert id into room_data
-                room['id']: str = room_id
-
-                # set new room KEY using room-id
-                local_rooms[room_id]: dict = room
-
-                # update room column
-                cursor.execute(f"UPDATE {self.table_name} SET room='{json.dumps(local_rooms)}' WHERE json_extract(account, '$.username')='{self.username}'")
-                database.commit()
-                database.close()
                 return {"result": room_generated_true, "rooms": local_rooms}
             else:
                 return authentication_result
-        # database/table does not exist
-        except sqlite3.OperationalError:
-            return {"result": account_exists_false}
+
+        except pymongo.errors.ConnectionFailure:
+            return {"result": "error connecting to mongodb database"}
 
     async def delete(self, room_id: str):
         try:
